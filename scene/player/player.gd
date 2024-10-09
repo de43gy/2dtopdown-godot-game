51e9623fc003dda8b06aa9c573638b7a306e92b7
Ko <de43gy@gmail.com>
2024-10-09 21:58:52 +0400

    Initial commit

---
+++ scene/player/player.gd
@@ -0,0 +1,17 @@
+extends CharacterBody2D
+
+var max_speed = 200
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	velocity = max_speed * calc_direction()
+	move_and_slide()
+
+func calc_direction ():
+	var movement_x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	var movement_y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
+	return Vector2(movement_x,movement_y).normalized()
---
+++ scene/slime/slime.gd
@@ -0,0 +1,19 @@
+extends CharacterBody2D
+
+var max_speed = 80
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	velocity = max_speed * get_direction_to_player()
+	move_and_slide()
+
+func get_direction_to_player():
+	var player = get_tree().get_first_node_in_group("player") as Node2D
+	if player != null:
+		return (player.global_position - self.global_position).normalized()
+	return Vector2.ZERO
