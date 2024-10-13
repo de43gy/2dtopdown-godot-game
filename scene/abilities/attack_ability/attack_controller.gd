39ca57ab44e046ceefd36292ede88b2d9a9b8e03
Ko <de43gy@gmail.com>
2024-10-14 02:56:22 +0400

    Calculation of the nearest enemy and attacking the nearest enemy.

--- scene/abilities/attack_ability/attack_controller.gd
+++ scene/abilities/attack_ability/attack_controller.gd
@@ -1,21 +1,29 @@
 extends Node
 
+var attack_range = 100
+
 @export var attack_ability: PackedScene
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
-	pass
-
 
 func _on_timer_timeout():
 	var player = get_tree().get_first_node_in_group("player") as Node2D
 	if player == null:
 		return
+	
+	var enemyes = get_tree().get_nodes_in_group("enemy")
+	
+	enemyes = enemyes.filter(func(enemy:Node2D):
+		return enemy.global_position.distance_squared_to(player.global_position) < pow(attack_range, 2)
+	)
+	
+	if enemyes.size() == 0:
+		return
+	
+	enemyes.sort_custom(func(a:Node2D, b:Node2D):
+		var a_distance = a.global_position.distance_squared_to(player.global_position)
+		var b_distance = b.global_position.distance_squared_to(player.global_position)
+		return a_distance < b_distance
+	)
+
 	var attack_instance = attack_ability.instantiate() as Node2D
 	player.get_parent().add_child(attack_instance)
-	attack_instance.global_position = player.global_position
+	attack_instance.global_position = enemyes[0].global_position
